b87fb421a06bc90b71ea58cdf7facd1ca915c028
Max Roberts <133835313+TheMaxRob@users.noreply.github.com>
2024-02-17 11:06:48 +0000

    Made buttons have optional ...props

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,7 +1,7 @@
 import { EXAMPLES } from '../data';
 import TabButton from './TabButton';
 import { useState } from 'react'
-
+import Section from './Section';
 
 export default function Examples() {
     function handleSelect(selectedButton) {
@@ -10,13 +10,12 @@ export default function Examples() {
     }
     let [ selectedTopic, setSelectedTopic ] = useState('');
     return (
-        <section id='examples'>
-          <h2>Examples</h2>
+        <Section title='Examples' id='examples'>
           <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
           </menu>
 
           { selectedTopic ? (<div id='tab-content'>
@@ -32,6 +31,6 @@ export default function Examples() {
               </code>
             </pre>
           </div>) : <p>Click a button to begin exploring these core topics!</p> }
-        </section>
+        </Section>
     );
 }
